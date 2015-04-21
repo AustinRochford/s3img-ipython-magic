76e048b581de16fbcbd270f6e6faa4ba11b27f19
AustinRochford <arochford@monetate.com>
2015-04-21 19:43:21 -0400

    Refactor S3 interactions for reusability

--- s3img_magic.py
+++ s3img_magic.py
@@ -2,15 +2,24 @@ from IPython.display import Image
 
 import boto
 
-def s3img(uri):
+def parse_s3_uri(uri):
     if uri.startswith('s3://'):
         uri = uri[5:]
 
-    bucket_name, key_name = uri.split('/', 1)
+    return uri.split('/', 1)
+
+
+def get_s3_key(uri):
+    bucket_name, key_name = parse_s3_uri(uri)
 
     conn = boto.connect_s3()
     bucket = conn.get_bucket(bucket_name)
-    key = bucket.get_key(key_name)
+
+    return bucket.get_key(key_name)
+
+
+def s3img(uri):
+    key = get_s3_key(uri)
     data = key.get_contents_as_string()
 
     return Image(data=data)
