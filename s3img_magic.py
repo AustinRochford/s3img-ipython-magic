b374221d8d0e902494066d666570c1a882c962bc
AustinRochford <arochford@monetate.com>
2015-04-21 20:27:13 -0400

    Add magic to save a Matplotlib figure to S3

--- s3img_magic.py
+++ s3img_magic.py
@@ -1,6 +1,10 @@
+from StringIO import StringIO
+
+from IPython.core.magic import Magics, magics_class, line_magic
 from IPython.display import Image
 
 import boto
+
 
 def parse_s3_uri(uri):
     if uri.startswith('s3://'):
@@ -9,13 +13,24 @@ def parse_s3_uri(uri):
     return uri.split('/', 1)
 
 
+def get_s3_bucket(bucket_name):
+    conn = boto.connect_s3()
+
+    return conn.get_bucket(bucket_name)
+
+
 def get_s3_key(uri):
     bucket_name, key_name = parse_s3_uri(uri)
-
-    conn = boto.connect_s3()
-    bucket = conn.get_bucket(bucket_name)
+    bucket = get_s3_bucket(bucket_name)
 
     return bucket.get_key(key_name)
+
+
+def get_or_create_s3_key(uri):
+    bucket_name, key_name = parse_s3_uri(uri)
+    bucket = get_s3_bucket(bucket_name)
+
+    return bucket.new_key(key_name)
 
 
 def s3img(uri):
@@ -25,5 +40,21 @@ def s3img(uri):
     return Image(data=data)
 
 
+@magics_class
+class S3ImageSaver(Magics):
+    @line_magic
+    def s3img_save(self, line):
+        """BEWARE: this magic will happily overwrite any S3 URI"""
+        fig_name, uri = line.split(' ', 1)
+
+        fig = self.shell.ev(fig_name)
+        tmp = StringIO()
+        fig.savefig(tmp)
+
+        key = get_or_create_s3_key(uri)
+        key.set_contents_from_string(tmp.getvalue())
+
+
 def load_ipython_extension(ipython):
     ipython.register_magic_function(s3img, 'line')
+    ipython.register_magics(S3ImageSaver)
