025523145d47208548136eee7409d9e51c76c681
AustinRochford <arochford@monetate.com>
2015-05-09 13:35:37 -0400

    Version 0.0.3

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ except ImportError:
 
 setup(
     name='s3img-ipython-magic',
-    version='0.0.2',
+    version='0.0.3',
     author="Austin Rochford",
     author_email="austin.rochford@gmail.com",
     description="s3 image line magic for ipython 2 and 3",
     install_requires=['ipython>=2.0', 'boto'],
     keywords="ipython",
     license="MIT",
-    long_description="""This package provides a line magic for embedding images from Amazon S3 into an IPython notebook""",
+    long_description="""This package provides a line magics for embedding images from Amazon S3 into an IPython notebook""",
     platforms='any',
     py_modules=['s3img_magic'],
     url="https://github.com/AustinRochford/s3img-ipython-magic",
